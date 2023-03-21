70cb3e325fd3f2303a0f73105b3ea8a8b88edf8a
Manh Do Company <bkhnbkhn1@gmail.com>
2023-03-21 16:21:28 +0700

    method references related to default(defender) method in interface java8

--- src/main/java/org/example/MethodReference.java
+++ src/main/java/org/example/MethodReference.java
@@ -6,35 +6,53 @@ interface Say {
 }
 
 interface Say2 {
-    void saySomething();
+    void saySomething(String msg);
 }
+
+interface Messageble {
+    Message getMessage(String msg);
+}
+
+class Message {
+
+    public Message(String msg) {
+        System.out.println(msg);
+    }
+}
+
 public class MethodReference {
     public static void sayAdditional() {
         System.out.println("hi, i'm references static method!!!");
     }
 
-    public void sayAdditional2() {
-        System.out.println("hi, i'm references instance method!!!");
+    public void sayAdditional2(String type) {
+        System.out.println("hi, i'm references instance method via " + type + " !!!");
     }
     public static void main(String[] args) {
-        // references static method
+        // 1 references static method
         // cung kieu tra ve, cung tham so
         Say say = MethodReference::sayAdditional;
         // calling interface method
         say.saySomething();
 
-        // references to an instance method
+        // 2 references to an instance method
+        // 2.1 via object
         // create object
         MethodReference methodReference = new MethodReference();
         // referring non-static method using references
         Say2 say2 = methodReference::sayAdditional2;
         // calling interface method
-        say2.saySomething();
+        say2.saySomething("object");
 
+        // 2.1 anonymous object
         // for short
         // referring non-static method using anonymous object
         Say2 say22 = new MethodReference()::sayAdditional2;
-        say22.saySomething();
+        say22.saySomething("anonymous object");
+
+        // 3 reference to a constructor
+        Messageble m = Message::new;
+        m.getMessage("hi, i'm references constructor!!!");
     }
 }
 
